b496003afc7f8eb9ef1781fccce1938ddfa5816f
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2021-12-06 22:59:37 +0000

    Now pulling flickr auth URL all the way back

--- public/js/flickr-auth.js
+++ public/js/flickr-auth.js
@@ -1,6 +1,27 @@
+async function redirectToFlickrAuth() {
+    const flickrUrlResponse = await fetch( 
+        "https://api.flickrgroupaddr.com/api/v1/auth/flickr/user_auth_url",
+        {
+            credentials: 'include',
+        }
+    );
+
+    const jsonResponse = await flickrUrlResponse.json();
+
+    console.log( "Flickr URL we're about to redirect to: " + jsonResponse["flickr_user_auth_url"] );
+}
+
+function attachEventListeners() {
+    const authButton = document.getElementById( "button_initiate_flickr_auth" );
+    authButton.addEventListener( "click", redirectToFlickrAuth );
+
+}
+
 function main() {
     console.log( "JS launched" );
 
-    confirmValidLogin();
+    // Attach any action listeners
+    attachEventListeners();
 }
+
 main();
--- public/js/ui-auth.js
+++ public/js/ui-auth.js
@@ -8,13 +8,17 @@ function confirmValidLogin() {
     const cognitoSubId = getCookie( 'FGA_COGNITO_SUB_ID' );
     const fgaSessionId = getCookie( 'FGA_SESSION_ID' );
 
-    console.log( "Cognito sub: " + cognitoSubId );
+    //console.log( "Cognito sub: " + cognitoSubId );
 
     // Valiues will be "undefined" if they're not in cookies
     if ( (!cognitoSubId) || (!fgaSessionId) ) {
         console.log( "Not authenticated, redirecting to Cognito login" );
+        window.location.replace( "https://login.flickrgroupaddr.com/login?client_id=hpc8p0fu5ke50v6ft8h1ti37f&response_type=code&scope=email+openid&redirect_uri=https://auth.flickrgroupaddr.com/oauth/callback" )
     } else {
-        console.log( "User Cognito ID: " + cognitoSubId );
-        console.log( "Session ID: " + fgaSessionId );
+        console.log( "Valid credentials!" );
+        console.log( "\tUser Cognito ID: " + cognitoSubId );
+        console.log( "\tSession ID: " + fgaSessionId );
     }
 }
+
+confirmValidLogin();
