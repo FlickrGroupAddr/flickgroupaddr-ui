9967ceadce04cf22904e6a17185bdca18eb0796e
Terry Ott <Terry.Ott@gmail.com>
2022-04-14 23:35:14 +0000

    Reboot and start clean

--- public/js/console/flickr-auth.js
+++ public/js/console/flickr-auth.js
@@ -1,13 +1,1 @@
-function haveAuthorizedWithFlickr() {
-    const fgaAccessToken = getFgaAuthCookie();
-
-    return false;
-}
-
-if ( haveAuthorizedWithFlickr === false ) {
-    console.log("Need to do Flickr auth");
-
-    document.getElementById("div_flickr_id").style.display = "block";
-} else {
-    console.log("We have flickr auth, display their name" );
-}
+console.log("How about this");
