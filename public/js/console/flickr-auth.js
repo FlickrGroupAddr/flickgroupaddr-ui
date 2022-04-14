42c1a3f248e50c34dfee3918a99ee63eb9548d8e
Terry Ott <Terry.Ott@gmail.com>
2022-04-14 23:38:15 +0000

    Baby step two

--- public/js/console/flickr-auth.js
+++ public/js/console/flickr-auth.js
@@ -1,1 +1,13 @@
-console.log("How about this");
+function haveAuthorizedWithFlickr() {
+    const fgaAccessToken = getFgaAuthToken();
+
+    return false;
+}
+
+if ( haveAuthorizedWithFlickr() === false ) {
+    console.log("Need to do Flickr auth");
+
+    document.getElementById("div_flickr_id").style.display = "block";
+} else {
+    console.log("Have flickr auth");
+} 
