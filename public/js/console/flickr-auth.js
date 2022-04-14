e240282162899d274bb25f39ba850bde32b86ead
Terry Ott <Terry.Ott@gmail.com>
2022-04-14 23:31:19 +0000

    Stuff

--- public/js/console/flickr-auth.js
+++ public/js/console/flickr-auth.js
@@ -2,12 +2,15 @@ function haveAuthorizedWithFlickr() {
 
     const fgaAccessToken = getFgaAuthCookie();
 
-    
-
+    return false;
 }
 
 const haveDoneFlickrAuth = haveAuthorizedUserWithFlickr();
 
-if ( haveCompletedFlickrAuth === false ) {
+if ( haveDoneFlickrAuth === false ) {
     console.log("Need to do Flickr auth");
+
+    document.getElementById("div_flickr_id").style.display = "block";
+} else {
+    console.log("We have flickr auth, display their name" );
 }
