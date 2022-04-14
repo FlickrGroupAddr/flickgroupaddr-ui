5bae19bf911d97d787946d30a3225ea1d72e2cd7
Terry Ott <Terry.Ott@gmail.com>
2022-04-14 23:54:40 +0000

    Push some new JS you fucker

--- public/js/console/flickr-auth.js
+++ public/js/console/flickr-auth.js
@@ -1,28 +1,4 @@
 const flickrCredsEndpoint = "https://x4etaszxrl.execute-api.us-east-2.amazonaws.com/api/v001/flickr_creds";
 
-/*
-async function haveAuthorizedWithFlickr() {
-    const fgaAccessToken = getFgaAuthToken();
-
-    let fetchResponse = await fetch( flickrCredsEndpoint, 
-        { 
-            headers: { 'Authorization': fgaAccessToken }
-        } );
-
-    let returnedJson = await fetchResponse.json();
-
-    return returnedJson['have_flickr_creds'];
-}
-*/
-
 console.log("Are we getting here");
 
-/*
-if ( haveAuthorizedWithFlickr() === false ) {
-    console.log("Need to do Flickr auth");
-
-    document.getElementById("div_flickr_auth").style.display = "block";
-} else {
-    console.log("Have flickr auth");
-} 
-*/
