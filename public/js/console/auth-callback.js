9feb0e88872811577b8d96cc4d77cbc43fee876e
Terry Ott <Terry.Ott@gmail.com>
2022-04-14 21:30:53 +0000

    Maybe setting a cookie and fixing the browser URL now

--- public/js/console/auth-callback.js
+++ public/js/console/auth-callback.js
@@ -1,7 +1,19 @@
 function processAccessToken( accessToken ) {
     console.log( "We found an access token in the window URL" );
-    console.log( "Access code: " + accessToken );
+    #console.log( "Access code: " + accessToken );
 
+    // Set the cookie 
+    document.cookie = "FGA_ACCESS_TOKEN=" + accessToken;
+
+    console.log( "FGA_ACCESS_TOKEN cookie set" );
+}
+
+
+function cleanUpUrl() {
+    // Overwrite the URL to remove all the code stuff
+    history.replaceState(null, null, "https://flickrgroupaddr.com/console" );
+
+    console.log( "URL rewritten to remove access token now that it's stored in a cookie" );
 }
 
 
@@ -11,4 +23,5 @@ const urlParams = new URLSearchParams(queryString);
 
 if ( urlParams.has( "access_token" ) === true ) {
     processAccessToken( urlParams.get("access_token") );
+    cleanUpUrl();
 }
