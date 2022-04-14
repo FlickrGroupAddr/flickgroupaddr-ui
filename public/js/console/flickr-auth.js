57fa888813011831b5fd5cf5dff10edc9c0ff4af
Terry Ott <Terry.Ott@gmail.com>
2022-04-14 23:51:15 +0000

    Made async function

--- public/js/console/flickr-auth.js
+++ public/js/console/flickr-auth.js
@@ -1,6 +1,6 @@
 const flickrCredsEndpoint = "https://x4etaszxrl.execute-api.us-east-2.amazonaws.com/api/v001/flickr_creds";
 
-function haveAuthorizedWithFlickr() {
+async function haveAuthorizedWithFlickr() {
     const fgaAccessToken = getFgaAuthToken();
 
     let fetchResponse = await fetch( flickrCredsEndpoint, 
