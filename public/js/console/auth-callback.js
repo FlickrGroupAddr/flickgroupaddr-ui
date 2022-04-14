e997db25379c3ff9807ef3fdf26c66bc44137a17
Terry Ott <Terry.Ott@gmail.com>
2022-04-14 21:11:06 +0000

    Starting work to detect access code param

--- public/js/console/auth-callback.js
+++ public/js/console/auth-callback.js
@@ -1,1 +1,14 @@
-console.log("Inside auth callback script" );
+function processAccessToken( accessToken ) {
+    console.log( "We found an access token in the window URL" );
+    console.log( "Access code: " + accessToken );
+
+}
+
+
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+if ( urlParams.has( "access_token" ) === true ) {
+    processAccessToken( urlParams.get("access_token") );
+}
