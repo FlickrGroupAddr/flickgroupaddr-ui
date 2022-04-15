e9ee7ecc5208bc892eace29e47bb61a143938c85
Terry Ott <Terry.Ott@gmail.com>
2022-04-15 00:15:58 +0000

    Maybe this I'm so lost

--- public/js/console/flickr-proxy.js
+++ public/js/console/flickr-proxy.js
@@ -3,15 +3,20 @@ console.log("Are we getting here");
 
 const haveFlickrCredsEndpoint = "https://x4etaszxrl.execute-api.us-east-2.amazonaws.com/api/v001/flickr_creds";
 
-async function checkForFlickrCreds() {
+async function checkForFlickrCreds( authToken ) {
 
-    let fetchResponse = await fetch( haveFlickrCredsEndpoint );
+    let fetchResponse = await fetch( haveFlickrCredsEndpoint,
+        {
+            headers: { "Authorization": authToken } 
+        });
     return false;
 
 }
 
 console.log("I'm getting bored");
 
-const haveFlickrCreds = checkForFlickrCreds();
+const authToken = getFgaAuthToken();
+
+const haveFlickrCreds = checkForFlickrCreds( authToken );
 
 console.log("Have flickr creds: " + haveFlickrCreds );
