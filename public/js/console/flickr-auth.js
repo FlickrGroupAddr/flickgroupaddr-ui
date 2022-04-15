73f4c080f0dfc4cd0d6c4458f1c633ef8d9636f3
Terry Ott <Terry.Ott@gmail.com>
2022-04-15 20:55:01 +0000

    That looked better. Let's enable the button div if we don't have creds

--- public/js/console/flickr-auth.js
+++ public/js/console/flickr-auth.js
@@ -1,26 +1,24 @@
-console.log("Hellow");
-console.log("Are we getting here");
-
 const haveFlickrCredsEndpoint = "https://x4etaszxrl.execute-api.us-east-2.amazonaws.com/api/v001/flickr_creds";
 
 
-async function checkForFlickrCreds( authToken ) {
+async function getFlickrIdentity( authToken ) {
 
     let fetchResponse = await fetch( haveFlickrCredsEndpoint,
         {
             headers: { "Authorization": authToken } 
         });
-    return false;
-}
 
-console.log("I'm getting bored");
+    const returnIdentity = null;
+
+    return returnIdentity;
+}
 
 const authToken = getFgaAuthToken();
 
-checkForFlickrCreds( authToken )
-                                .then( haveFlickrCreds => {
-                                    console.log("Have flickr creds: " + haveFlickrCreds );
-                                })
-                                .catch( err => {
-                                    console.log("Exception thrown during creds check");
-                                });
+getFlickrIdentity( authToken ).then( returnedIdentity => {
+    if ( returnedIdentity === null ) {
+        document.getElementById("div_flickr_auth").style.display = "block";
+    }
+}).catch( err => {
+    console.log("Exception thrown during creds check");
+});
