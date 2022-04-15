027f028a77adc4a703e21014137b15207355141c
Terry Ott <Terry.Ott@gmail.com>
2022-04-15 23:15:55 +0000

    New auth, a 404 from flickr_id endpoint is legit

--- public/js/console/flickr-auth.js
+++ public/js/console/flickr-auth.js
@@ -1,3 +1,4 @@
+const fgaLoginUrl = "https://flickrgroupaddr.auth.us-east-2.amazoncognito.com/login?client_id=54om78s59usqmo8nqdr7m7bktg&response_type=code&scope=email+openid&redirect_uri=https://x4etaszxrl.execute-api.us-east-2.amazonaws.com/oauth/callback"
 const flickrIdEndpoint = "https://x4etaszxrl.execute-api.us-east-2.amazonaws.com/api/v001/flickr_id";
 
 
@@ -33,7 +34,18 @@ async function getFlickrIdentity( authToken ) {
             headers: { "Authorization": authToken } 
         });
 
-    const returnIdentity = null;
+    if ( fetchResponse.ok ) {
+        const returnIdentity = await fetchResponse.json();
+    } else {
+        if ( fetchResponse.status === 401 ) {
+            console.log( "Credentials have expired" );
+            location.href = fgaLoginUrl; 
+        }
+        else if ( fetchResponse.status === 404 ) {
+            console.log( "User does not have an associated Flickr ID" );
+            const returnIdentity = null;
+        } 
+    }
 
     return returnIdentity;
 }
