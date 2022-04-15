8b8f58d0e9f2241f4f1a778430c0ce93fd5becac
Terry Ott <Terry.Ott@gmail.com>
2022-04-15 00:13:08 +0000

    Trying a new file name

---
+++ public/js/console/flickr-proxy.js
@@ -0,0 +1,17 @@
+console.log("Hellow");
+console.log("Are we getting here");
+
+const haveFlickrCredsEndpoint = "https://x4etaszxrl.execute-api.us-east-2.amazonaws.com/api/v001/flickr_creds";
+
+function checkForFlickrCreds() {
+
+    let fetchResponse = await fetch( haveFlickrCredsEndpoint );
+    return false;
+
+}
+
+console.log("I'm getting bored");
+
+const haveFlickrCreds = checkForFlickrCreds();
+
+console.log("Have flickr creds: " + haveFlickrCreds );
