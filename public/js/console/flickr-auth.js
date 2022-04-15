d3a6bcb63d5ec5448bccfe9e9d5fa8c55aeeaf11
Terry Ott <Terry.Ott@gmail.com>
2022-04-15 21:20:49 +0000

    Event listener for flickr auth button

--- public/js/console/flickr-auth.js
+++ public/js/console/flickr-auth.js
@@ -1,6 +1,18 @@
 const haveFlickrCredsEndpoint = "https://x4etaszxrl.execute-api.us-east-2.amazonaws.com/api/v001/flickr_creds";
 
 
+function initiateFlickrAuth() {
+    console.log( "Button to kick to Flickr auth clicked" );
+
+}
+
+
+function addEventListeners() {
+    document.getElementById("button_initiate_flickr_auth").addEventListener( "click",
+        initiateFlickrAuth );
+}
+
+
 async function getFlickrIdentity( authToken ) {
 
     let fetchResponse = await fetch( haveFlickrCredsEndpoint,
@@ -13,12 +25,19 @@ async function getFlickrIdentity( authToken ) {
     return returnIdentity;
 }
 
+addEventListeners();
+
+
 const authToken = getFgaAuthToken();
 
 getFlickrIdentity( authToken ).then( returnedIdentity => {
     if ( returnedIdentity === null ) {
         console.log( "No associated flickr ID, display section to create an association" )
+        document.getElementById("div_flickr_id").style.display = "block";
         document.getElementById("div_flickr_id_none").style.display = "block";
+    }
+    else {
+        // TODO: display username & user NSID
     }
 }).catch( err => {
     console.log("Exception thrown during creds check");
