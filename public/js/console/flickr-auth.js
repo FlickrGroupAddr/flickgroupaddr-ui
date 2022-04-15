b0e5622e3b7ff95eb2d80ac4fd3077df2cdbabc4
Terry Ott <Terry.Ott@gmail.com>
2022-04-15 00:20:32 +0000

    Last try I'm so done

--- public/js/console/flickr-auth.js
+++ public/js/console/flickr-auth.js
@@ -3,19 +3,14 @@ console.log("Are we getting here");
 
 const haveFlickrCredsEndpoint = "https://x4etaszxrl.execute-api.us-east-2.amazonaws.com/api/v001/flickr_creds";
 
-function checkForFlickrCreds( authToken ) {
-    return await thisOtherThis( authToken );
-}
 
-
-async function thisOtherShit( authToken ) {
+async function checkForFlickrCreds( authToken ) {
 
     let fetchResponse = await fetch( haveFlickrCredsEndpoint,
         {
             headers: { "Authorization": authToken } 
         });
     return false;
-
 }
 
 console.log("I'm getting bored");
