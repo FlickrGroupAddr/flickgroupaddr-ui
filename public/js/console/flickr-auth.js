5af22451718910461b73afc35fd5e359c8254d32
Terry Ott <Terry.Ott@gmail.com>
2022-04-15 00:09:28 +0000

    what the shit

--- public/js/console/flickr-auth.js
+++ public/js/console/flickr-auth.js
@@ -3,12 +3,7 @@ console.log("Are we getting here");
 
 const haveFlickrCredsEndpoint = "https://x4etaszxrl.execute-api.us-east-2.amazonaws.com/api/v001/flickr_creds";
 
-async function checkForFlickrCreds() {
-    const fetchResponse = await fetch( haveFlickrCredsEndpoint,
-        {
-            headers: { "Authorization": getFgaAuthToken } 
-        }
-    );
+function checkForFlickrCreds() {
 
     return false;
 
