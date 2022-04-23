0ef88ca1ff2c5de474b75653cfd87f44dc529292
Terry Ott <Terry.Ott@gmail.com>
2022-04-23 22:05:07 +0000

    Trying to make first pull on API endpoint for user requests

--- public/js/console/get-user-requests.js
+++ public/js/console/get-user-requests.js
@@ -1,1 +1,31 @@
+const userRequestsUrl = "https://x4etaszxrl.execute-api.us-east-2.amazonaws.com/api/v001/flickr/user/requests"
+
+async function getUserRequests() {
+   const fetchResponse = await fetch( userRequestsUrl,
+        {
+            headers: {
+                "Authorization": getFgaAuthToken()
+            },
+
+            method: "GET"
+        });
+
+    const jsonBody = await fetchResponse.json();
+
+    console.log( "Got the following content back:\n" + JSON.stringify(jsonBody) );
+
+    return jsonBody['user_requests'];
+
+
+}
+
+function showUserRequests( userRequests ) {
+    console.log( "Showing user requests" );
+
+}
+
 console.log("Loaded get user request script");
+
+userRequests = getUserRequests();
+
+showUserRequests( userRequests );
